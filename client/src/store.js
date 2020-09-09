084d02d72622441405e520e6c5fb6bc76ad521c4
Ben Rodia <br425@drexel.edu>
2020-09-09 09:37:34 -0400

    Re-add package lock / delete vestigial public files

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,10 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux"
+import {createStore, combineReducers, applyMiddleware, compose} from "redux"
 import thunk from "redux-thunk"
-import reducer from './reducers'
-import initialState from './constants/initialState'
+import reducer from "./reducers"
+import initialState from "./constants/initialState"
 
 const middleware = [thunk]
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reducer,initialState,composeEnhancers(applyMiddleware(...middleware)))
-
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, initialState)
 
 export default store
