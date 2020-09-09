ab5d7fd988895c2721a2f30fffd3ec742b09d1e7
Ben Rodia <br425@drexel.edu>
2020-09-09 07:08:28 -0400

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const config = require("config")
-
+const path = require("path")
 const app = express()
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"))
+
+	app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")))
+}
 const port = process.env.PORT || 5000
 
 app.use(express.json())
